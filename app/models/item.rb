c8e4b6c48f3ff4ac46a837cd3523d58da04c117c
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-10 19:44:20 +0900

    Activehashのデータ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :delivery_burden, :shipping_origin, :arrival_day,
+
+  validates :item, :image, :text, :category, :status, :delivery_burden, :shipping_origin, :arrival_day, :price, presence: true
+
+  validates :category_id, :status_id, :delivery_burden_id, :shipping_origin_id, :arrival_day_id, numericality: { other_than: 1 }
+  
 end

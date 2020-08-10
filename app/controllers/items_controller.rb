b4b29f968709c9b5f6370fafb5aaeb65fdd2f34a
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-10 16:54:26 +0900

    items#newのコントローラーの記入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: []
+  before_action :authenticate_user!, only: [:new]
 
 
   def index
     @items = Item.all.order("created_at DESC")
+  end
+
+  def new
+    @items = Item.new
   end
 
 end

fa698868a41f899819ca91d7044562725a54afb0
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-10 12:27:43 +0900

    可読性の向上

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root 'items#index'
   resources :items, only: :index
 
 
-  root 'items#index'
+
   
 
 end

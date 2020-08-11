03df62bf588deffb9016634bed9551bfc60873cd
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-11 18:11:43 +0900

    商品出品時データ保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: []
 
 
   def index
@@ -7,7 +7,25 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+  end
+
+  def create
+    
+    @item = Item.create(item_params)
+     
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item, :image, :text, :category_id, :status_id, :delivery_burden_id, :shipping_origin_id, :arrival_day_id, :price)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new, :create]
+  # post 'items/new', to: 'items#create'
   
 end

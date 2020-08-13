0fff3d4e7b0fcd5f81c680f4979659a51860c663
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-13 21:46:47 +0900

    脱字修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
-      it "priceが9999999以上だと出品できない" do
+      it "priceが9999999円以上だと出品できない" do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")

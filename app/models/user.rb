617e8b5b1b954539962f18648c994c2d1fe0e10e
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-14 18:04:45 +0900

    uaerのvalidates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,8 @@ class User < ApplicationRecord
   has_many :purchases
 
   with_options presence: true do
-    validates :nickname, :email, :passwaold, :passwaold_confirmation, :familyname_kanji, :firstname_kanji, :familyname_katakana, :firstname_katakana, :birthday
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, length: { maximum: 255 },
-              format: { with: VALID_EMAIL_REGEX }
+    validates :nickname, :email, :encrypted_passwaold, :passwaold_confirmation, :familyname_kanji, :firstname_kanji, :familyname_katakana, :firstname_katakana, :birthday
 
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
-    validates :passworad, { minimum: 6 },
-              format: { with: VALID_PASSWORD_REGEX  }
-    validates :passworad, confirmation: true
 
     VALID_NAME_KAJI_REGEX = /\A(?=.*?[ぁ-んァ-ン一-龥])\z/
     validates :familyname_kanji, format: { with: VALID_NAME_KAJI_REGEX }

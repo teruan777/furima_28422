d25c047adc0785edd143b13c7a0da1fc770c8180
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-15 00:03:08 +0900

    テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,6 +39,7 @@ RSpec.describe User, type: :model do
       end
       it "passwoldが6文字を満たしてない場合新規登録できない" do
         @user.password = "12345"
+        @user.password_cnfirmation = "12345"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end

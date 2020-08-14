567594afcfb78411087230efe983ff34eba2aa2d
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-14 12:23:14 +0900

    validationの指定変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_origin
   belongs_to_active_hash :arrival_day
 
-  with_options on: :create? do
-    validates :item, :image, :text, :category, :status, :delivery_burden, :shipping_origin, :arrival_day, :price, presence: true
+  with_options presence: true do
+    validates :item, :image, :text, :category, :status, :delivery_burden, :shipping_origin, :arrival_day
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :category_id, :status_id, :delivery_burden_id, :shipping_origin_id, :arrival_day_id, numericality: { other_than: 1 }
   end

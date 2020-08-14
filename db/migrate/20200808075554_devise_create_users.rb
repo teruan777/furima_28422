6ad211cbe24097b0eb149394c0f67475b55213e7
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-14 18:06:36 +0900

    余分なカラムを削除
    ・それに伴い一部記述修正

--- db/migrate/20200808075554_devise_create_users.rb
+++ db/migrate/20200808075554_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,              null: false
       t.string :email,                 null: false, default: ""
       t.string :encrypted_password,    null: false, default: ""
-      t.string :password_confirmation, null: false
       t.string :familyname_kanji,      null: false
       t.string :firstname_kanji,       null: false
       t.string :familyname_katakana,   null: false

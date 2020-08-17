84190664ec6db17d600a7bc2e72a2ad781f6995a
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-17 18:00:14 +0900

    with_oputionにてvalidatesの可読性向上

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,20 +9,22 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, :email, :password, :familyname_kanji, :firstname_kanji, :familyname_katakana, :firstname_katakana, :birthday
 
-    VALID_EMAIL_REGEX = /\A\S+@\S+\.\S+\z/.freeze
-    validates :email, format: { with: VALID_EMAIL_REGEX }
-    validates :email, format: { with: VALID_EMAIL_REGEX }
+    with_options format: { with: /\A\S+@\S+\.\S+\z/.freeze } do
+      validates :email
+    end
 
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/.freeze
-    validates :password, format: { with: VALID_PASSWORD_REGEX }
-    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/.freeze } do
+      validates :password
+    end
 
-    VALID_NAME_KANJI_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/.freeze
-    validates :familyname_kanji, format: { with: VALID_NAME_KANJI_REGEX }
-    validates :firstname_kanji, format: { with: VALID_NAME_KANJI_REGEX }
-
-    VALID_NAME_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-    validates :familyname_katakana, format: { with: VALID_NAME_KATAKANA_REGEX }
-    validates :firstname_katakana, format: { with: VALID_NAME_KATAKANA_REGEX }
+    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/.freeze } do
+      validates :familyname_kanji
+      validates :firstname_kanji
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/.freeze } do
+      validates :familyname_katakana
+      validates :firstname_katakana
+    end
   end
 end

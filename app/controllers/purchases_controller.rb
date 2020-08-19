22dd1f52bb2627ddd9eb7bce2c3b40bb994931f9
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-19 17:04:45 +0900

    クレジットカード決済機能実装（修正）

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,9 +3,10 @@ class PurchasesController < ApplicationController
   def create
     @purchase = Purchase.new(#: purchase_params[:#])
     if @purchase.valid?
+      @item = Item.find(pamams[:item_id])
       pay_item
       @purchase.save
-      return redirect_to #_path
+      return redirect_to item_path(@item.id)
     else
       render '#'
     end
@@ -13,13 +14,13 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    prams.permit(:token)
+    prams.permit(:token).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
     payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     payjp::Charge.create(
-      amount: purchase_params[:#],
+      amount: @item.price,
       card: purchase_params[:token],
       currency:'jpy'
     )

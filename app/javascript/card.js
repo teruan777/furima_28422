b972eb841ae9dd2c3ec104f98657465d943b9dd8
teruan777 <takuansamurai@yahoo.co.jp>
2020-08-21 21:57:28 +0900

    クレジットカード決済機能実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,8 @@
 const pay = () => {
+
+
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // PAY.JPテスト公開鍵
-  console.log(process.env.PAYJP_PUBLIC_KEY)
+
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -9,18 +11,18 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("card-number"),
-      cvc: formData.get("card-cvc"),
-      exp_month: formData.get("card-exp-month"),
-      exp_year: `20${formData.get("card-exp-year")}`,
-
+      number: formData.get("purchase_house_add[number]"),
+      cvc: formData.get("purchase_house_add[cvc]"),
+      exp_month: formData.get("purchase_house_add[exp_month]"),
+      exp_year: `20${formData.get("purchase_house_add[exp_year]")}`,
     };
 
-    Payjp.createToken(card, (status, response) => {
-      if (status === 200) {
+  Payjp.createToken(card, (status, response) => {
+
+    if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        const tokenObj = `<input value=${token} type="hidden" name='purchase_house_add[token]'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
 
         document.getElementById("card-number").removeAttribute("name");
@@ -31,8 +33,13 @@ const pay = () => {
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
       } else {
-        alert("card情報が間違ってます")
-            }
+        alert("Token can't be blank")
+        const input = document.getElementById("itemID");
+        const itemID = input.getAttribute('data-id')
+        console.log(itemID) 
+        location.href= `/items/${itemID}/purchases`;
+
+      }
     });
   });
 };
---
+++ spec/models/house_add_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe HouseAdd, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end

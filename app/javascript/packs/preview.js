4c1b4272116e392125c4be3c7ddf81b98d7890bb
teruan777 <takuansamurai@yahoo.co.jp>
2020-09-18 21:46:43 +0900

    商品プレビュー機能実装

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,25 @@
+if (window.location.href.match(/\/items\/new/) || window.location.href.match(/\/items\/\d+\/edit/)){
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    const ItemImage = document.getElementById('item-image');
+    ItemImage.addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      
+      createImageHTML(blob);
+    });
+  });
+}

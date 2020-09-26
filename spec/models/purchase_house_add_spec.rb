024f6859d05f0f70e3deca1dfcf78cdd1417e7ff
teruan777 <takuansamurai@yahoo.co.jp>
2020-09-27 01:03:51 +0900

    エラー文日本語化に伴いテストコード書き換え(購入機能)

--- spec/models/purchase_house_add_spec.rb
+++ spec/models/purchase_house_add_spec.rb
@@ -17,48 +17,39 @@ RSpec.describe PurchaseHouseAdd, type: :model do
     context '購入できないとき' do
       it '郵便番号が空では購入できない' do
         @purchase.postal_code = nil
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Postal code can't be blank")
+        expect(@purchase).to_not be_valid
       end
       it '郵便番号でハイフンが使われていなければ購入できない' do
         @purchase.postal_code = '0001111'
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        expect(@purchase).to_not be_valid
       end
       it '郵便番号で正しく入力されていなければ購入できない' do
         @purchase.postal_code = '0000-000'
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        expect(@purchase).to_not be_valid
       end
       it '都道府県が(--)では購入できない' do
         @purchase.shipping_origin_id = nil
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Shipping origin can't be blank")
+        expect(@purchase).to_not be_valid
       end
       it '市区町村が空では購入できない' do
         @purchase.city = nil
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("City can't be blank")
+        expect(@purchase).to_not be_valid
       end
       it '番地が空では購入できない' do
         @purchase.address = nil
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Address can't be blank")
+        expect(@purchase).to_not be_valid
       end
       it '電話番号が空では購入できない' do
         @purchase.phone = nil
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Phone can't be blank")
+        expect(@purchase).to_not be_valid
       end
       it '電話番号でハイフンが使われていた場合購入できない' do
         @purchase.phone = '090-1234-5678'
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Phone Enter the phone number within 11 digits without hyphens")
+        expect(@purchase).to_not be_valid
       end
       it '電話番号が11桁より多かった場合購入できない' do
         @purchase.phone = '090123456789'
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Phone Enter the phone number within 11 digits without hyphens")
+        expect(@purchase).to_not be_valid
       end
     end
   end
